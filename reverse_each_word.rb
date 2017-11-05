40d6f8c1ab10819534b92d6d1d9ae0de81968f73
Joey Cho <cjy12182000@naver.com>
2017-11-05 20:34:30 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,10 +2,9 @@ def reverse_each_word(string)
 
   arr = string.split
 
-  arr.collect do |variable|
-    variable.reverse!
-    arr.push(variable)
-  end
+#  arr.collect do |variable|
+#    variable.reverse!
+#    arr.push(variable)
+#  end
   return arr
 end
-
